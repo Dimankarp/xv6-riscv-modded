c9f93642e2dd63c80532665c9e97aaa498dc7c9d
Dimankarp <marinad1814@yandex.ru>
2024-11-29 22:39:06 +0300

    feat(proc test): Added test for allocation of large number of procs

--- user/proctest.c
+++ user/proctest.c
@@ -8,144 +8,35 @@
 #include "kernel/memlayout.h"
 #include "kernel/riscv.h"
 
-// concurrent forks to try to expose locking bugs.
-void
-sbrkmuch(char *s)
-{
-  enum { BIG=125*1024*1024 };
-  char *c, *oldbrk, *a, *lastaddr, *p;
-  uint64 amt;
-
-  oldbrk = sbrk(0);
-
-  // can one grow address space to something big?
-  a = sbrk(0);
-  amt = BIG - (uint64)a;
-  p = sbrk(amt);
-  if (p != a) {
-    printf("%s: sbrk test failed to grow big address space; enough phys mem?\n", s);
-    exit(1);
-  }
-
-  // touch each page to make sure it exists.
-  char *eee = sbrk(0);
-  for(char *pp = a; pp < eee; pp += 4096)
-    *pp = 1;
-
-  lastaddr = (char*) (BIG-1);
-  *lastaddr = 99;
-
-  // can one de-allocate?
-  a = sbrk(0);
-  c = sbrk(-PGSIZE);
-  if(c == (char*)0xffffffffffffffffL){
-    printf("%s: sbrk could not deallocate\n", s);
-    exit(1);
-  }
-  c = sbrk(0);
-  if(c != a - PGSIZE){
-    printf("%s: sbrk deallocation produced wrong address, a %p c %p\n", s, a, c);
-    exit(1);
-  }
-
-  // can one re-allocate that page?
-  a = sbrk(0);
-  c = sbrk(PGSIZE);
-  if(c != a || sbrk(0) != a + PGSIZE){
-    printf("%s: sbrk re-allocation failed, a %p c %p\n", s, a, c);
-    exit(1);
-  }
-  if(*lastaddr == 99){
-    // should be zero
-    printf("%s: sbrk de-allocation didn't really deallocate\n", s);
-    exit(1);
-  }
-
-  a = sbrk(0);
-  c = sbrk(-(sbrk(0) - oldbrk));
-  if(c != a){
-    printf("%s: sbrk downsize failed, a %p c %p\n", s, a, c);
-    exit(1);
-  }
-}
-
-void
-manyforks(char *s)
-{
-  // int count = 50;
+#define PROC_NUM 120
+void manyforks() {
+  int pids[PROC_NUM];
   int i = 0;
   int pid = 1;
-  while(pid != 0 && i++ < 120)
+  while (pid != 0 && i < PROC_NUM) {
     pid = fork();
-  if(pid < 0){
-    printf("%s: fork failed", s);
-    exit(1);}
-  if(pid == 0){
-    exit(0);
+    if (pid < 0) {
+      printf("fork failed at i : %d", i);
+      exit(1);
+    }
+    if (pid == 0) {
+      for (;;)
+        ;
+    }
+    pids[i++] = pid;
+  }
+  for(int j = 0; j < i; j++){
+    kill(pids[j]);
+  }
+    for(int j = 0; j < i; j++){
+    wait(0);
   }
   return;
 }
 
-void
-preempt(char *s)
-{
-  char buf[255];
-  int pid1, pid2, pid3;
-  int pfds[2];
-
-  pid1 = fork();
-  if(pid1 < 0) {
-    printf("%s: fork failed", s);
-    exit(1);
-  }
-  if(pid1 == 0)
-    for(;;)
-      ;
-
-  pid2 = fork();
-  if(pid2 < 0) {
-    printf("%s: fork failed\n", s);
-    exit(1);
-  }
-  if(pid2 == 0)
-    for(;;)
-      ;
-
-  pipe(pfds);
-  pid3 = fork();
-  if(pid3 < 0) {
-     printf("%s: fork failed\n", s);
-     exit(1);
-  }
-  if(pid3 == 0){
-    close(pfds[0]);
-    if(write(pfds[1], "x", 1) != 1)
-      printf("%s: preempt write error", s);
-    close(pfds[1]);
-    for(;;)
-      ;
-  }
-
-  close(pfds[1]);
-  if(read(pfds[0], buf, sizeof(buf)) != 1){
-    printf("%s: preempt read error", s);
-    return;
-  }
-  close(pfds[0]);
-  kill(pid1);
-  kill(pid2);
-  kill(pid3);
-  wait(0);
-  wait(0);
-  wait(0);
-}
-
-int main(){
-    for(int i = 0; i < 100; i++)
-      preempt("preempt");
-    sbrk(int)
-    printf("ended fork\n");
-    sbrkmuch("sbrkmuch");
-    printf("ended sbrk\n");
-        exit(0);
+int main() {
+  printf("[manyforks]: testing forks of %d procs\n", PROC_NUM);
+  manyforks();
+  printf("[manyforks]: test passed\n");
+  exit(0);
 }
